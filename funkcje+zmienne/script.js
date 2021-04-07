08b9e29c343c159d8486d655eeed56fa4a20eb6d
KubaSzewczyk <80067084+KubaSzewczyk@users.noreply.github.com>
2021-04-07 13:14:59 +0200

    Add files via upload

---
+++ funkcje+zmienne/script.js
@@ -0,0 +1,112 @@
+1.
+const name = "Karol"
+const age = 40
+const isOpen = true
+const stringBoolean = "true"
+const stringNumber = 100
+
+2.
+console.log("Michał")
+console.log("HelloAnia")
+console.log(4)
+console.log(false)
+
+3.
+console.log(name)
+console.log(age)
+console.log(isOpen)
+console.log(stringBoolean)
+console.log(stringNumber)
+
+4.
+console.log(2+2)
+console.log("Michał" + "Kowalski")
+console.log(2+2*2)
+
+5.
+const example = 55+55
+console.log(example)
+
+6.
+let example2 = "przykład"
+let example2 = "przykład2"
+console.log(example2)
+
+7.
+a) function modulo(a, b){
+   return result = 6%4}
+b) function Hello(){
+   return result = "Hello world"}
+c) function isOpen(){
+   return result = true}
+d) function modulo(a, b){
+   return result = 6%4}
+e) function power(a, b){
+   return result = 3**4}
+f) function Hello(a, b){
+   return result = "Hello, " + "how are you?"}
+g) function multiple(a, b){
+   return result = 5*60}
+h) function multiple2(a, b){
+   return result = 3*3600}
+i) function divide(a, b, c){
+   return result = 10 * 9 / 2} 
+j) function minus(a, b){
+   return result = 200-10}
+
+8.
+a) let arr = [1,2,3]
+b) let cars = ["Tesla", "Toyota", "BMW", "Mercedes", "Jeep"]
+c) let bool = [true, false, true]
+d) let blank = []
+e) let mix = [true, 10, "kot", false, "słoń"]
+f) console.log(mix[1])
+g) let test2 = [1,2,3]
+   console.log(test2.length)
+h) let test = []
+   test.push("pies") 
+   console.log(test)
+
+9.
+a) let obj = {}
+b) let person = {
+   name: "Iza"}
+c) let personDetails = {
+   name: "Kuba",
+   lastName: "Szewczyk",
+   age: 27,
+   drivingLicence: true}
+d) console.log(personDetails["age"])
+e) console.log(personDetails)
+
+10.
+a) function sum (number1, number2){
+   return number1 + number2}   
+b) function inscription(word){
+   return word}
+c) function boolean(value){
+   return value}
+d) function modulo(number1, number2){
+   return number1 % number2}
+e) function power(a,b){
+   return a**b}
+f) function strings(string, string2){
+   return string + string2}
+g) function minToSec(minutes){
+   return minutes*60}
+h) function hrToSec(hours){
+   return hours*3600}
+i) function triangle(a, b){
+   return a * b * 0.5}
+j) function yearsToDays(years){
+   return years*365}
+k) function power(a){
+   return a**a}
+l) function longerSide(side1, side2){
+   return (side1 + side2) - 1}
+m) function sayHello(name){
+   return "cześć " + name}
+n) function countAge(dateOfBirthday){
+   return 2021 - dateOfBirthday} 
+o) function scores(a, b){
+   return (a*2) + (b*3)}
